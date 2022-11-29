6c3cb4948da9c99569f1e563db763203d5d11789
TribalScale <alimansour97@gmail.com>
2022-11-28 21:31:50 -0700

    Feat/ sidebar styling

--- online-fe/src/components/sidebar/index.js
+++ online-fe/src/components/sidebar/index.js
@@ -1,7 +1,33 @@
 import "./sidebar.css";
 
 const Sidebar = () => {
-  return <div className="sidebar-container"></div>;
+  return (
+    <div className="sidebar-container">
+      <nav>
+        <li>My feed</li>
+        <ul>
+          <li>Popular</li>
+          <li>Most upvoted</li>
+          <li>Best discussions</li>
+          <li>Search</li>
+        </ul>
+        <ul>
+          <li>Bookmarks</li>
+          <li>Reading history</li>
+          <li>Toggle theme</li>
+        </ul>
+      </nav>
+      <div className="empty-space"></div>
+      <nav className="bottom-nav">
+        <ul>
+          <li>Popular</li>
+          <li>Most upvoted</li>
+          <li>Best discussions</li>
+          <li>Search</li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidebar;
